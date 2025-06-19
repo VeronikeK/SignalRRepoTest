2e1d067fbefd0e0c123d50ab9f315eba8223b63d
Veronike <veronike.kitchen@gmail.com>
2025-06-19 09:31:50 +0200

    000-setup code

--- SignalRRepoTest/Program.cs
+++ SignalRRepoTest/Program.cs
@@ -1,7 +1,10 @@
+using SignalRChat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
